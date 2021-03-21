f5b5f10f90403784b91e242e63ca1a55bff4e8ed
Sarika Pawar <jadhavsarikb.gmail.com>
2021-03-21 18:03:06 +0400

    <add new file>

---
+++ map_constructor.dart
@@ -0,0 +1,10 @@
+void main() {
+  var details = new Map();
+  details['Usrname'] = 'admin';
+  details['Password'] = 'admin@123';
+  print(details);
+
+  //foreach
+  var usrMap = {"name": "Tom", 'Email': 'tom@xyz.com'};
+  usrMap.forEach((k, v) => print('${k}: ${v}'));
+}
